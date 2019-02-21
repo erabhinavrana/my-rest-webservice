b4034599a30b4153b9cba52c7ddb67d52e22fb4e
Abhinav <erabhinav.rana@gmail.com>
2019-02-21 13:43:15 +0400

    Updating the Document information of Restful services using Docket inbuilt methods.

--- src/main/java/com/abhi/webservices/myrestwebservice/configuration/SwaggerConfig.java
+++ src/main/java/com/abhi/webservices/myrestwebservice/configuration/SwaggerConfig.java
@@ -2,9 +2,15 @@ package com.abhi.webservices.myrestwebservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Abhinav on 2/20/2019.
@@ -12,8 +18,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    private static final Contact DEFAULT_CONTACT = new Contact("Abhinav Rana", "http://www.abhinavrana.com", "erabhinav.rana@gmail.com");
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+            "Restful Service Documentation",
+            "Documentation for the designed restful services",
+            "1.0", "Term of Service URL for the service, if any",
+            DEFAULT_CONTACT, "Apache 2.0",
+            "http://www.apache.org/licenses/LICENSE-2.0");;
+    private static final Set<String> DEFAULT_PRODUCES_CONSUMES = new HashSet<>(Arrays.asList("application/json", "application/xml"));
+
     @Bean
     public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_CONSUMES);
     }
 }
--- src/main/java/com/abhi/webservices/myrestwebservice/model/Employee.java
+++ src/main/java/com/abhi/webservices/myrestwebservice/model/Employee.java
@@ -1,20 +1,26 @@
 package com.abhi.webservices.myrestwebservice.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
-
+@ApiModel(description = "Employee Details")
 public class Employee {
     private Integer id;
     @NotNull
     @Size(min = 3)
+    @ApiModelProperty(notes = "Name should contains minimum 3 characters.")
     private String name;
     @NotNull
     @Size(min = 3, max = 15)
+    @ApiModelProperty(notes = "Designation should contains minimum 3 and maximum 15 characters.")
     private String designation;
     @Past
+    @ApiModelProperty(notes = "Date of Birth shouldn't be the future date.")
     private LocalDate dateOfBirth;
 
     public Employee(Integer id, String name, String designation, LocalDate dateOfBirth) {
