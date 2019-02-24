f6283051c12c13fcb40b605c60af83faa8527758
Abhinav <erabhinav.rana@gmail.com>
2019-02-24 16:20:52 +0400

    Implement advance versioning using params, headers or produces on restful services.

--- src/main/java/com/abhi/webservices/myrestwebservice/controller/StudentVersioning.java
+++ src/main/java/com/abhi/webservices/myrestwebservice/controller/StudentVersioning.java
@@ -12,13 +12,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentVersioning {
 
-    @GetMapping("/v1/student")
+    @GetMapping("/student/v1")
     public StudentV1 getStudentNameV1(){
         return new StudentV1("Test versioning");
     }
 
-    @GetMapping("/v2/student")
+    @GetMapping("/student/v2")
     public StudentV2 getStudentNameV2(){
         return new StudentV2(new StudentName("Test", "Versioning"));
     }
+
+    @GetMapping(value="/student/param", params="version=1")
+    public StudentV1 getStudentNameByParamV1(){
+        return new StudentV1("Test versioning");
+    }
+
+    @GetMapping(value="/student/param", params = "version=2")
+    public StudentV2 getStudentNameByParamV2(){
+        return new StudentV2(new StudentName("Test", "Versioning"));
+    }
+
+    @GetMapping(value="/student/header", headers ="x-api-version=1")
+    public StudentV1 getStudentNameByHeaderV1(){
+        return new StudentV1("Test versioning");
+    }
+
+    @GetMapping(value="/student/header", headers = "x-api-version=2")
+    public StudentV2 getStudentNameByHeaderV2(){
+        return new StudentV2(new StudentName("Test", "Versioning"));
+    }
+
+    @GetMapping(value="/student/produces", produces ="application/vnd.company.app-v1+json")
+    public StudentV1 getStudentNameByProducesV1(){
+        return new StudentV1("Test versioning");
+    }
+
+    @GetMapping(value="/student/produces", produces = "application/vnd.company.app-v2+json")
+    public StudentV2 getStudentNameByProducesV2(){
+        return new StudentV2(new StudentName("Test", "Versioning"));
+    }
+
 }
